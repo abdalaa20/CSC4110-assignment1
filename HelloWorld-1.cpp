5f0384160666eec0f707130642d54e7c1d000382
abdalaa20 <hl4371@wayne.edu>
2025-01-24 18:40:06 -0500

    Adding author name

--- HelloWorld-1.cpp
+++ HelloWorld-1.cpp
@@ -1,6 +1,7 @@
-// Simple C++ program to display "Hello World" 
-// Header file for input output functions 
-#include<iostream>  
+// Simple C++ program to display "Hello World"
+// Author: Abdalaa <hl4371@wayne.edu>
+// Header file for input output functions
+#include<iostream>
   
 using namespace std; 
   
